fb6cb37fdf5d5ba271446c7e88fef9a9d6b3cc51
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-24 03:13:06 +0500

    Update app to add greeting component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Greeting from './components/Greeting';
 
-function App() {
-  return (
-    <div className="App">
-      Learn React
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Greeting />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
